94247cd03ffcee9897ef96a59458a1c97317ff53
Alena <elen.malets@gmail.com>
2021-05-09 14:31:18 +0200

    two of the sending request with the same start dare

--- src/services.ts
+++ src/services.ts
@@ -1,5 +1,9 @@
 import { generateRandomData } from './data/requests'
 
+const getRandomInt = (max: number) => {
+    return (Math.floor(Math.random() * max)) + 1;
+}
+
 /**
  * Get requests based on skills
  * @param skills
@@ -8,12 +12,12 @@ export const getRequests = async (
     skills: string[]
 ) => {
     const requestsData = generateRandomData()
-    // console.log('requestsData', requestsData)
     try {
         const filteredRequests = requestsData.filter((item: { skills: string[]; }) => {
             return item.skills.some(r=> skills.includes(r))
         }).slice(0, 5)
-        // console.log('filteredRequests', filteredRequests)
+        const number = getRandomInt(4)
+        filteredRequests[number].startDate = filteredRequests[0].startDate
         return filteredRequests
     } catch (error) {
         console.log('error', error)
